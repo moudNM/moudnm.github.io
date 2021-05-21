31bb24a43922760360a9b2e6f8a5848bde46a5ff
Nur Muhammad <moud@Mouds-Mac.local>
2021-05-21 21:06:38 +0800

    fix all responsiveness

--- js/email.js
+++ js/email.js
@@ -5,7 +5,10 @@ window.onload = function() {
     // these IDs from the previous steps
     emailjs.sendForm('service_w7pl1do', 'template_ryhatlh', this)
       .then(function() {
+        location.replace("contact-sent.html")
+        console.log('Change page!');
         console.log('SUCCESS!');
+
       }, function(error) {
         console.log('FAILED...', error);
       });
